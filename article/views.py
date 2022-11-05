b494cba5159204b177959f0278e4309fdd6da188
mohammadpagard <mohammadpagard2003@gmail.com>
2022-11-05 16:46:36 +0330

    Add permission for article and comment detail(Owner or Read only).

--- article/views.py
+++ article/views.py
@@ -1,8 +1,10 @@
 # DRF packages
 from rest_framework import generics
-from .models import Article, Comment
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 # Local apps
+from .models import Article, Comment
 from . import serializers
+from . import permissions
 
 
 class ArticleListView(generics.ListAPIView):
@@ -10,11 +12,13 @@ class ArticleListView(generics.ListAPIView):
     serializer_class = serializers.ArticleListSerializer
 
 
-class ArticleDetailView(generics.RetrieveDestroyAPIView):
+class ArticleDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = serializers.ArticleDetailSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, permissions.IsOwnerOrReadOnly)
 
 
 class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = serializers.CommentDetailSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, permissions.IsOwnerOrReadOnly)
