e903a1b36784145a063e7ce9bda98e8a9d3ebbb7
mohammadpagard <mohammadpagard2003@gmail.com>
2022-11-06 09:46:49 +0330

    Refactor(article:permissions) for super user/

--- article/permissions.py
+++ article/permissions.py
@@ -6,4 +6,4 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.owner == request.user
+        return obj.owner == request.user or request.user.is_superuser
